0516e17dee6f837795d0c4baca51851f3d3ddbbd
“Vincent <vlin6@stuy.edu>
2017-12-10 22:20:17 -0500

    forgot to add this

--- 04ReversedCharSeq/ReversedCharSeq.java
+++ 04ReversedCharSeq/ReversedCharSeq.java
@@ -1,3 +1,33 @@
 public class ReversedCharSeq{
+    private String Seq;
+
+    public ReversedCharSeq(String s) {
+	String temp = "";
+	for (int i = s.length() - 1; i >= 0; i --){
+	    temp += s.charAt(i);
+	}
+        Seq = temp;
+    }
+
+    public char charAt(int i){
+	return Seq.charAt(i);
+    }
+
+    public int length(){
+	return Seq.length();
+    }
+
+    public ReversedCharSeq subSequence(int start, int end){
+	String temp = "";
+	for (int i = end - 1; i >= start; i --){
+	    temp += Seq.charAt(i);    
+	}
+	ReversedCharSeq sub = new  ReversedCharSeq(temp);
+	return sub;
+    }
+
+    public String toString(){
+	return Seq;
+    }
 
 }
