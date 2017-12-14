37b11d7f2b4445eea2bb37ffbfeb9ae27bb039fd
“Vincent <vlin6@stuy.edu>
2017-12-13 22:01:06 -0500

    got it to work

--- 09GUI/TempConvert.java
+++ 09GUI/TempConvert.java
@@ -1,12 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-public class TempConvert{
+public class TempConvert extends JFrame implements ActionListener{
     
     private Container pane;
     private JButton b;
-    private JLabel l;
-    private JCheckBox c;
+    private JLabel l, l2;
+    private JCheckBox c, f;
     private JTextField t;
     
     public static double CtoF(double c){
@@ -17,13 +17,31 @@ public class TempConvert{
 	return ((f - 32) * 5) / 9;
     }
 
+    public void actionPerformed(ActionEvent e){
+        String s = e.getActionCommand();
+        System.out.println(s);
+        if(s.equals("ByteMe")){
+            //button code here
+            if( c.isSelected() ){
+                t.setText(t.getText().toUpperCase());
+            }else{
+                t.setText("What do you want?");
+            }
+        }
+        if(s.equals("Clear")){
+            //button code here
+            t.setText("");
+        }
+
+    }
+
     public TempConvert(){
 	this.setTitle("Temperature Converter");
-	this.setSize(800,600);
+	this.setSize(300,150);
 	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 	pane = this.getContentPane();
-	pane.setlayout(new FlowLayout());
+	pane.setLayout(new FlowLayout());
 	b = new JButton("Convert");
 	l = new JLabel("Enter Temp Here:");
 	c = new JCheckBox("Celsius to Fahrenheit");
@@ -31,12 +49,22 @@ public class TempConvert{
 	l2 = new JLabel("which one?");
 	t = new JTextField(5);
 
+	b.addActionListener(this);
+	c.addActionListener(this);
+        f.addActionListener(this);
+        t.addActionListener(this);
+
 	pane.add(l);
-	pane.add(l);
-	pane.add(l);
-	pane.add(l);
-	pane.add(l);
-	
+	pane.add(t);
+	pane.add(b);
+	pane.add(l2);
+	pane.add(c);
+	pane.add(f);
      }
+
+    public static void main(String[] args) {
+        TempConvert g = new TempConvert();
+        g.setVisible(true);
+    }
     
 }
