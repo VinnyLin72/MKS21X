81b62484a9c02d5a6492a30ddb2c7ae2ffa479b8
“Vincent <vlin6@stuy.edu>
2018-01-04 22:15:58 -0500

    made changes

--- 01SuperArray/SuperArray.java
+++ 01SuperArray/SuperArray.java
@@ -7,8 +7,14 @@ public class SuperArray{
         size = 0;
     }
 
+    public SuperArray(int capacity){
+	data = new String[capacity];
+	size = 0;
+    }
+
     public void clear(){
 	data = new String[size];
+	size = 0;
     }
 
     public int size(){
@@ -16,10 +22,10 @@ public class SuperArray{
     }
 
     public boolean add(String thing){
-        resize();
-        data[size-1] = thing;
+        add(size, thing);
         return true;
     }
+
     public String toString(){
         String output = "";
 	for (int i = 0; i < data.length; i ++){
@@ -27,27 +33,29 @@ public class SuperArray{
         }
         return "[" + output.substring(0, output.length() - 2) + "]";
     }
+
     public String get(int i){
         if (i < size && i > 0)return data[i];
-        return "Error: Index out of range";
+        throw new IndexOutOfBoundsException();
     }
+
     public String set(int i, String s){
         String out = data[i];
         if (i < size && i > 0)data[i] = s;
         return out;
     }
+
     public boolean isEmpty(){
-        for (int i = 0; i < data.length; i ++){
-            if (data[i] != null) return false;
-        }
-        return true;
+	return size == 0;
     }
+
     private void resize(){
 	size ++;
-        String[] newdata = new String[size];
+        String[] newdata = new String[2 * size + 1];
         System.arraycopy(data, 0 , newdata, 0, size - 1);
         data = newdata;
     }
+
     public boolean contains(String thing){
 	for (int i = 0; i < size; i ++){
 	    if (data[i] == thing) return true;
@@ -67,32 +75,35 @@ public class SuperArray{
 	return -1;
     }
     public void add(int i, String thing){
-	if (i < 0 || i > size)System.out.println("Error: Index out of range");
+	if (i < 0 || i > size) throw new IndexOutOfBoundsException();
 	else{
-	    resize();
+	    if (size == data.length) resize();
 	    for (int in = size-1; in > i; in --){
 		data[in]=data[in - 1];
 	    }
 	    data[i] = thing;
+	    size ++;
 	}
     }
+
     public String remove(int i){
-	if (i < 0 || i > size)return "Error: Index out of range";
+	if (i < 0 || i > size)throw new IndexOutOfBoundsException();
 	String ans = data[i];
 	int place = i;
 	for (int in = i+1; in < size; in ++){
 	    data[place] = data[in];
 	    place ++;
 	}
-	String[] newdata = new String[size-1];
-        System.arraycopy(data, 0 , newdata, 0, size - 1);
-        data = newdata;
+	size --;
 	return ans;
     }
     public boolean remove(String thing){
-	int i = indexOf(thing);
-	if (remove(i) != "Error: Index out of range"){
-	return true;
+	int index = indexOf(thing);
+       	for (int i = 0; i < size; i ++){
+	    if (i == index){
+		remove(i);
+		return true;
+	    }
 	}
 	return false;
     }
